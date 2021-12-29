8016fb555a2d845df51d4413a2eca395e86dbd3a
Freddy <freddyvelarde59@gmail.com>
2021-12-28 20:55:06 -0400

    project ends

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 
-const URI_DB = `mongodb://localhost:27017/image_uploader_db`
+// const URI_DB = `mongodb://localhost:27017/image_uploader_db`
+const URI_DB = `mongodb+srv://freddy:${process.env.KEY_MDB}@cluster0.qklvm.mongodb.net/image_uploader_db?retryWrites=true&w=majority`
 const config = {
   useNewUrlParser: true,
   useUnifiedTopology: true
